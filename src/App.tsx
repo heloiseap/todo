0d49a3e6fe5ea75e3ff719ac1ab0186ed3a73b6d
Heloise <heloiseap@users.noreply.github.com>
2025-11-27 11:54:49 -0300

    Text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Text from "./components/text"
+
 export default function App() {
 
   return (
-    <>
+    <Text variant="body-sm-bold" className="text-pink-base">
       Olá mundo!
-    </>
+    </Text>
   )
 }
 
