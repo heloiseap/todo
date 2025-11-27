6db55d0ad0032c32cd77d314feaa2fc411a95236
Heloise <heloiseap@users.noreply.github.com>
2025-11-27 14:42:50 -0300

    Badge component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Text from "./components/text"
 import TrashIcon from "./assets/icons/trash.svg?react"
 import Icon from "./components/icon"
+import Badge from "./components/badge"
 
 export default function App() {
 
   return (
     <div className="grud gap-3">
-      
+
       <Text variant="body-sm-bold" className="text-pink-base">
         Olá mundo!
       </Text>
@@ -14,6 +15,11 @@ export default function App() {
       <div>
         <Icon svg={TrashIcon} className="fill-green-base" />
         {/* <TrashIcon className="fill-pink-base"/> */}
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
       </div>
 
     </div>
