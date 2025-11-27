f6ed652b25df5889e1ce903527f4005327a7655a
Heloise <heloiseap@users.noreply.github.com>
2025-11-27 14:26:48 -0300

    Icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import Text from "./components/text"
+import TrashIcon from "./assets/icons/trash.svg?react"
+import Icon from "./components/icon"
 
 export default function App() {
 
   return (
-    <Text variant="body-sm-bold" className="text-pink-base">
-      Olá mundo!
-    </Text>
+    <div className="grud gap-3">
+      
+      <Text variant="body-sm-bold" className="text-pink-base">
+        Olá mundo!
+      </Text>
+
+      <div>
+        <Icon svg={TrashIcon} className="fill-green-base" />
+        {/* <TrashIcon className="fill-pink-base"/> */}
+      </div>
+
+    </div>
   )
 }
 
