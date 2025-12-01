b70fe278c2c30a0f0d1d49a0962b39e3ec04a4e5
Heloise <heloiseap@users.noreply.github.com>
2025-12-01 14:16:11 -0300

    Preparar Tarefa

--- src/core-components/task-list.tsx
+++ src/core-components/task-list.tsx
@@ -2,15 +2,20 @@ import Button from "../components/button";
 import PlusIcon from "../assets/icons/plus.svg?react"
 import TaskItem from "./task-item";
 import useTasks from "../hooks/use-tasks";
+import useTask from "../hooks/use-task";
 
 export default function TaskList() {
     const { tasks } = useTasks()
-    console.log(tasks)
+    const { prepareTask } = useTask()
+
+    function handleNewTask() {
+        prepareTask()
+    }
 
     return (
         <>
         <section>
-            <Button icon={PlusIcon} className="w-full">
+            <Button icon={PlusIcon} className="w-full" onClick={handleNewTask}>
                 Nova Tarefa
             </Button>
         </section>
