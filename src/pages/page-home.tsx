bd660cf19a837eb64f52c261cd4b09df4cf90725
Heloise <heloiseap@users.noreply.github.com>
2025-12-01 10:58:41 -0300

    task list component

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,5 +1,5 @@
 import Container from "../components/container";
-import TaskItem from "../core-components/task-item";
+import TaskList from "../core-components/task-list";
 import TasksSummary from "../core-components/tasks-summary";
 
 
@@ -9,8 +9,8 @@ export default function PageHome() {
         <header className="flex items-center justify-between">
             <TasksSummary />
         </header>
-
-        <TaskItem />
+        
+        <TaskList />
     </Container>
 
 }
